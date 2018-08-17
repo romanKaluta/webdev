b201e2d7bd32dae15013a3607d88c80d8eb09c14
romanKaluta <KalutaR.I@gmail.com>
2018-08-18 00:29:22 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+function changeon1(pic1) {
+	pic1.src = 'img/offers/hover1.png'
+}
+function changeout1(pic1) {
+	pic1.src = 'img/offers/1.png'
+}
+
+function changeon2(pic2) {
+	pic2.src = 'img/offers/hover2.png'
+}
+function changeout2(pic2) {
+	pic2.src = 'img/offers/2.png'
+}
+
+function changeon3(pic3) {
+	pic3.src = 'img/offers/hover3.png'
+}
+function changeout3(pic3) {
+	pic3.src = 'img/offers/3.png'
+}
+
+function changeon4(pic4) {
+	pic4.src = 'img/offers/hover4.png'
+}
+function changeout4(pic4) {
+	pic4.src = 'img/offers/4.png'
+}
+
+function changeon5(pic5) {
+	pic5.src = 'img/offers/hover5.png'
+}
+function changeout5(pic5) {
+	pic5.src = 'img/offers/5.png'
+}
+
+function changeon6(pic6) {
+	pic6.src = 'img/offers/hover6.png'
+}
+function changeout6(pic6) {
+	pic6.src = 'img/offers/6.png'
+}
+
+
+function changeon7(icon1) {
+	icon1.src = 'img/sotial_network/linkedin_active.png'
+}
+function changeout7(icon1) {
+	icon1.src = 'img/sotial_network/linkedin.png'
+}
+function changeon8(icon2) {
+	icon2.src = 'img/sotial_network/twiter_active.png'
+}
+function changeout8(icon2) {
+	icon2.src = 'img/sotial_network/twitter.png'
+}
+function changeon9(icon3) {
+	icon3.src = 'img/sotial_network/fb_active.png'
+}
+function changeout9(icon3) {
+	icon3.src = 'img/sotial_network/fb.png'
+}
